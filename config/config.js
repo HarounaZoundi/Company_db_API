a035e53d89a3b929c772f9755df60cd66b221b8f
Harouna Zoundi <harouna.zoundi1@gmail.com>
2020-06-26 03:31:32 -0400

    setting up db on cloud

--- config/config.js
+++ config/config.js
@@ -1,9 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect:  process.env.DB_TYPE
+// running locally
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect:  process.env.DB_TYPE
+// });
+
+const sequelize = new Sequelize("heroku_2ef692aeb03cd61", "b943aacbcbcf98","3e440962", {
+    host:"us-cdbr-east-02.cleardb.com",
+    dialect:  "mysql"
   });
   
   module.exports = sequelize; 
